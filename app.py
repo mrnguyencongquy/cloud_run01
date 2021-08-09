654ab2e81b0dfe80e5af6c4847e1b05cee6a5fea
Nguyen Cong Quy <mrnguyencongquy@gmail.com>
2021-08-09 11:28:06 +0700

    Data_check demo CSV

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask,Response,jsonify, render_template ,logging,request
+from flask import Flask, render_template, request
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+
+
 app = Flask(__name__)
-
-@app.route('/')
+# homepage
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    return render_template('index.html') 
 
-#run server
+# data display
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    if request.method == 'POST':
+        # read CSV
+        file_data = []
+        file_data = request.files['csvfile']
+        if file_data:
+            csv_data = pd.read_csv(file_data, encoding='utf8')
+ 
+            # type of columns
+            print(csv_data.dtypes)
+            # summarize the central tendency, dispersion 
+            # and shape of a dataset’s distribution
+            # print(csv_data.describe())
+    return render_template('data.html', data = csv_data.dtypes) 
+
 if __name__ == '__main__':
+    # app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
